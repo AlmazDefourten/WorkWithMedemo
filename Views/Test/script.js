f1123df6068c8425fcc7d617a42939655272ee9e
Nikolaev Nikita <69678682+NikitkaN@users.noreply.github.com>
2021-12-18 19:54:20 +0300

    New

---
+++ Views/Test/script.js
@@ -0,0 +1,13 @@
+async function sendJSON(e) {
+    e.preventDefault();
+    const response = await fetch("/api/user", {
+        method: "POST",
+        headers: { "Accept": "application/json", "Content-Type": "application/json" },
+        body: JSON.stringify({
+            name: document.getElementById("name").value,
+            lastname: document.getElementById("lastname").value
+        })
+    });
+    const message = await response.json();
+    document.getElementById("message").innerText = message.text;
+    }
