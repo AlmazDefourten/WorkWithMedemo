793ae6fdbd30c6fb81649a014695eb78b81add7f
AlmazDefourten <almaz.sahabiev@outlook.com>
2021-12-19 17:34:30 +0300

    fix

--- Views/script.js
+++ Views/script.js
@@ -1,3 +1,14 @@
-/*let data = JSON.stringify({ "name":
-name.value, "lastname": lastname.value });
-*/
+document.getElementById("sendBtn").addEventListener("click", sendJSON);
+async function sendJSON(e) {
+    e.preventDefault();
+    const response = await fetch("/api/user", {
+        method: "POST",
+        headers: { "Accept": "application/json", "Content-Type": "application/json" },
+        body: JSON.stringify({
+            name: document.getElementById("name").value,
+            lastname: document.getElementById("lastname").value
+        })
+    });
+    const message = await response.json();
+    document.getElementById("message").innerText = message.text;
+}
