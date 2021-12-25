37b8783f34e023e9a90b45af510897ba96e0d611
AlmazDefourten <almaz.sahabiev@outlook.com>
2021-12-25 14:58:33 +0300

    UserModel created

---
+++ WorkWithMe/WorkWithMe/Models/User.cs
@@ -0,0 +1,30 @@
+﻿namespace WorkWithMe.Models
+{
+    public class User
+    {
+        public string Nick;
+        public string? Name;
+        public string? LastName;
+
+        public string Password; // Для логина используется email
+
+        public string Email;
+        public bool isEmailShowing;
+
+        public string? PhoneNumber;
+        public bool? isNumberShowing;
+
+        public string? Country;
+        public string? City;
+
+        public DateTime? DateOfBirth;
+
+        public bool? isMale;
+
+        // Ссылки на соцсети
+        public string? VKLink;
+        public string? TelegramNick;
+
+        Dictionary<int, string>? Tags;
+    }
+}
